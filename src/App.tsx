f4975cf10a53076c6b2b8cd9d7143c659dc975dd
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-07-06 17:52:58 -0300

    Salvamento do menu no Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,28 @@ import {
     Navbar,
     NavbarItem,
 } from "./styles";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
+
+import standardMenu from './utils/standard_menu.json';
+const MENU = "MENU";
 
 function App() {
     const [isEditModalopened, setIsEditModalOpened] = useState<boolean>(false);
     const [itemToEditIndex, setItemToEditIndex] = useState<number>(0);
     const [newName, setNewName] = useState<string>("");
-    const [menu, setMenu] = useState([
-        {
-            name: "Início",
-        },
-        {
-            name: "Sobre nós",
-        },
-        {
-            name: "Contato",
-        },
-    ]);
+    const [menu, setMenu] = useState<any>([]);
+
+    useEffect(() => {
+        let menu = JSON.parse(localStorage.getItem(MENU)!);
+        if (menu !== null) {
+            setMenu(menu);
+        } else {
+            setMenu([{name: "Início",},{name: "Sobre nós",},{name: "Contato", },]);
+
+            localStorage.setItem(MENU, JSON.stringify(standardMenu));
+        }
+    }, []);
 
     const handleEditButtonClick = (index: number) => {
         setNewName(menu[index].name);
@@ -36,24 +41,27 @@ function App() {
 
     const handleSaveButtonClick = () => {
         setIsEditModalOpened(false);
-        menu[itemToEditIndex].name = newName;
         setItemToEditIndex(0);
         setNewName("");
+
+        menu[itemToEditIndex].name = newName;
+        localStorage.setItem(MENU, JSON.stringify(menu));
     };
     const handleEnterClick = (e: React.KeyboardEvent<HTMLDivElement>) => {
-        console.log("Método chamado");
         if (e.keyCode === 13) {
             setIsEditModalOpened(false);
-            menu[itemToEditIndex].name = newName;
             setItemToEditIndex(0);
             setNewName("");
+
+            menu[itemToEditIndex].name = newName;
+            localStorage.setItem(MENU, JSON.stringify(menu));
         }
     };
 
     return (
         <Container>
             <Navbar>
-                {menu.map((menuItem: any, i) => (
+                {menu.map((menuItem: any, i: number) => (
                     <NavbarItem key={i}>
                         {menuItem.name}
                         <EditItemButton
